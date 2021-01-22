a1e85df38061ae6e52e53f0220872360508f002f
mengwei <mengwei@liepin.com>
2021-01-22 18:27:12 +0800

    init

---
+++ project-security/src/main/java/com/mfanw/demo/project/DemoProjectApplication.java
@@ -0,0 +1,18 @@
+package com.mfanw.demo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消费端
+ *
+ * @author mengwei
+ */
+@SpringBootApplication
+public class DemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectApplication.class, args);
+    }
+
+}
---
+++ project-security/src/main/java/com/mfanw/demo/project/config/MyBatisConfig.java
@@ -0,0 +1,14 @@
+//package com.mfanw.helloworld.nacos.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * MyBatis相关配置
+// * Created by macro on 2019/4/8.
+// */
+//@Configuration
+//@EnableTransactionManagement
+//@MapperScan({"com.macro.mall.mapper", "com.macro.mall.dao"})
+//public class MyBatisConfig {
+//}
+//
